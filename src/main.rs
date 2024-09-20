e1f7c9abbca583290a2d24708e735967decf9716
Aniruddha Atale <aniruddha.atale@klaviyo.com>
2024-09-20 11:35:37 -0400

    Responding with 200/OK

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
+
+fn handle_client(mut stream: std::net::TcpStream) -> std::io::Result<()> {
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes())?;
+    stream.flush()?;
+    stream.shutdown(std::net::Shutdown::Both)?;
+    Ok(())
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,8 +19,9 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                handle_client(stream).unwrap_or_else(|_x| println!("error handling client"));
             }
             Err(e) => {
                 println!("error: {}", e);
