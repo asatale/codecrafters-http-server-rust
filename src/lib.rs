a90e31dc12496281cd1a6e1e71d721310fcd8cf0
Aniruddha Atale <aniruddha.atale@klaviyo.com>
2024-09-22 12:59:34 -0400

    Respond with 200 or 404 depending on url content

---
+++ src/lib.rs
@@ -0,0 +1,154 @@
+
+use std::{collections::HashMap, io::{Error, ErrorKind}};
+
+#[derive(Debug)]
+pub struct Request {
+    pub version: String,
+    pub method: String,
+    pub url: String,
+    pub headers: HashMap<String, String>,
+    pub _body: String,
+}
+
+enum ParseState {
+    StartLine,
+    Headers,
+    Body,
+}
+
+impl Request {
+    pub fn new() -> Request {
+        Request {
+            version: String::new(),
+            method: String::new(),
+            url: String::new(),
+            headers: HashMap::new(),
+            _body: String::new(),
+        }
+    }
+
+    fn readline(mut reader: impl FnMut(&mut [u8]) -> std::io::Result<usize>) -> std::io::Result<String>{
+        let mut buf = Vec::new();
+        let mut possible_end = false;
+        loop {
+            let mut byte = [0];
+            reader(&mut byte)?;
+            buf.push(byte[0]);
+            match byte[0] {
+                b'\n' if possible_end => break,
+                b'\r' => possible_end = true,
+                _ => possible_end = false,
+            }
+        }
+        let s = String::from_utf8(buf).unwrap();
+        Ok(s)
+    }
+
+    fn parse_request_line(&mut self, line: &str) -> std::io::Result<()> {
+        let mut tokens =  line.split_whitespace();
+
+        let str: &str = match tokens.next() {
+            Some(str) => str,
+            None => return Err(Error::new(ErrorKind::InvalidInput, "Invalid input")),
+        };
+        self.method = str.to_string();
+
+        let str: &str = match tokens.next() {
+            Some(str) => str,
+            None => return Err(Error::new(ErrorKind::InvalidInput, "Invalid input")),
+        };
+        self.url = str.to_string();
+
+        let str: &str = match tokens.next() {
+            Some(str) => str,
+            None => return Err(Error::new(ErrorKind::InvalidInput, "Invalid input")),
+        };
+        self.version = str.to_string();
+        Ok(())
+    }
+
+    fn parse_header(&mut self, line: &str) -> std::io::Result<()> {
+        let mut parts = line.splitn(2, ':');
+        let key = parts.next().unwrap().trim().to_string();
+        let value = parts.next().unwrap().trim().to_string();
+        self.headers.insert(key, value);
+        Ok(())
+    }
+
+    pub fn from_stream(mut reader: impl FnMut(&mut [u8]) -> std::io::Result<usize>) -> std::io::Result<Self> {
+        let mut state: ParseState = ParseState::StartLine;
+        let mut req = Request::new();
+
+        while let Ok(line) = Request::readline(&mut reader) {
+            match state {
+                ParseState::StartLine => {
+                    let _ = req.parse_request_line(&line)
+                    .or_else(|e| return Err(e));
+                    state = ParseState::Headers;
+                },
+                ParseState::Headers => {
+                    if line == "\r\n" {
+                        if req.method == "GET" {
+                            break;
+                        }
+                        state = ParseState::Body;
+                    } else {
+                        let _ = req.parse_header(&line)
+                        .or_else(|e| return Err(e));
+                    }
+                },
+                ParseState::Body => { break;},
+            }
+        }
+        Ok(req)
+    }
+}
+
+pub struct Response {
+    version: String,
+    status: String,
+    headers: HashMap<String, String>,
+    body: String,
+}
+
+impl Response {
+    pub fn new() -> Response {
+        Response {
+            version: String::new(),
+            status: String::new(),
+            headers: HashMap::new(),
+            body: String::new(),
+        }
+    }
+    pub fn set_version(&mut self, version: &str) {
+        self.version.push_str(version);
+    }
+    pub fn set_status(&mut self, status: &str) {
+        self.status.push_str(status);
+    }
+    pub fn set_header(&mut self, name: &str, value: &str) {
+        self.headers.insert(name.to_string(), value.to_string());
+    }
+
+    pub fn to_string(&self) -> std::io::Result<String> {
+        let mut msg = String::new();
+        msg.push_str(&self.version);
+        msg.push_str(" ");
+        msg.push_str(&self.status);
+        msg.push_str("\r\n");
+
+        for (key,value) in self.headers.iter() {
+            msg.push_str(&key);
+            msg.push_str(": ");
+            msg.push_str(&value);
+            msg.push_str("\r\n");
+        }
+        msg.push_str("Content-Length: ");
+        msg.push_str(&self.body.len().to_string());
+        msg.push_str("\r\n");
+        msg.push_str("\r\n");
+
+        Ok(msg)
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,49 @@
-// Uncomment this block to pass the first stage
-use std::{io::Write, net::TcpListener};
+use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
+use http_server_starter_rust::{Request as HttpRequest, Response as HttpResponse};
 
-fn handle_client(mut stream: std::net::TcpStream) -> std::io::Result<()> {
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+// Send response string to the client
+fn send_response(stream: &mut std::net::TcpStream, response: &str) -> std::io::Result<()> {
     stream.write(response.as_bytes())?;
     stream.flush()?;
-    stream.shutdown(std::net::Shutdown::Both)?;
+    Ok(())
+}
+
+// Send 200 OK response to the client
+fn send_ok(stream: &mut TcpStream, request: HttpRequest) -> std::io::Result<()> {
+    let mut response: HttpResponse = HttpResponse::new();
+    response.set_version(&request.version);
+    response.set_status(&"200 OK");
+    send_response(stream,&response.to_string().unwrap())
+}
+
+// Send 404 Not Found response to the client
+fn send_not_found(stream: &mut TcpStream, request: HttpRequest)-> std::io::Result<()> {
+    let mut response: HttpResponse = HttpResponse::new();
+    response.set_version(&request.version);
+    response.set_status(&"404 Not Found");
+    send_response(stream,&response.to_string().unwrap())
+}
+
+fn handle_request(stream: &mut TcpStream) -> std::io::Result<HttpRequest> {
+    let reader = |b: &mut[u8]| {stream.read(b)};
+    HttpRequest::from_stream(reader)
+}
+
+
+fn handle_client(stream: &mut TcpStream) -> std::io::Result<()> {
+    let result: Result<HttpRequest, std::io::Error> = handle_request(stream);
+    match result {
+        Ok(request) => {
+            if request.url == "/" {
+                send_ok(stream, request)?;
+            } else {
+                send_not_found(stream, request)?;
+            }
+        }
+        Err(e) => {
+            println!("Error parsing request: {}", e);
+        }
+    }
     Ok(())
 }
 
@@ -15,13 +53,14 @@ fn main() {
 
     // Uncomment this block to pass the first stage
 
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:4221").expect("Failed to create listener");
     
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => {
-                println!("accepted new connection");
-                handle_client(stream).unwrap_or_else(|_x| println!("error handling client"));
+            Ok(mut stream) => {
+                handle_client(&mut stream).unwrap_or(());
+                stream.shutdown(std::net::Shutdown::Both)
+                    .unwrap_or_else(|e| println!("error: {} in closing connection", e));
             }
             Err(e) => {
                 println!("error: {}", e);
