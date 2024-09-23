0103a6374b69cea791d1b45286d909eb047aafb0
Aniruddha Atale <aniruddha.atale@klaviyo.com>
2024-09-23 19:50:38 -0400

    Refactored

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 
-use std::{collections::HashMap, io::{Error, ErrorKind}};
+use std::{collections::HashMap, io::{Write, Read, Error, ErrorKind}};
 
 #[derive(Debug)]
 pub struct Request {
@@ -69,7 +69,11 @@ impl Request {
     fn parse_header(&mut self, line: &str) -> std::io::Result<()> {
         let mut parts = line.splitn(2, ':');
         let key = parts.next().unwrap().trim().to_string();
-        let value = parts.next().unwrap().trim().to_string();
+        let mut value = parts.next().unwrap().trim().to_string();
+
+        if self.headers.contains_key(&key) {
+            value = format!("{},{}", self.headers.get(&key).unwrap(), value).to_string();
+        }
         self.headers.insert(key, value);
         Ok(())
     }
@@ -120,14 +124,11 @@ pub struct Response {
 impl Response {
     pub fn new() -> Response {
         Response {
-            version: String::new(),
+            version: "HTTP/1.1".to_string(),
             status: String::new(),
             headers: HashMap::new(),
             body: String::new(),
         }
-    }
-    pub fn set_version(&mut self, version: &str) {
-        self.version.push_str(version);
     }
     pub fn set_status(&mut self, status: &str) {
         self.status.push_str(status);
@@ -161,3 +162,139 @@ impl Response {
     }
 }
 
+
+struct Transaction {
+    request: Request,
+    response: Response,
+}
+
+impl Transaction {
+    pub fn new(request: Request, response: Response) -> Transaction {
+        Transaction {
+            request: request,
+            response: response,
+        }
+    }
+}
+
+pub struct SessionConfig {
+    pub download_dir: String,
+    pub upload_dir: String,
+}
+
+pub struct Session {
+    stream: std::net::TcpStream,
+    config: SessionConfig,
+}
+
+impl Session {
+    pub fn new(config: SessionConfig, stream: std::net::TcpStream) -> Session {
+        println!("New session from: {}", stream.peer_addr().unwrap());
+        Session {
+            stream: stream,
+            config: config,
+        }
+    }
+
+    pub fn close(&mut self) -> std::io::Result<()> {
+        self.stream.shutdown(std::net::Shutdown::Both)?;
+        Ok(())
+    }
+
+    fn send(&mut self, response: &str) -> std::io::Result<()> {
+        self.stream.write(response.as_bytes())?;
+        self.stream.flush()?;
+        Ok(())
+    }
+
+    fn receive(&mut self) -> std::io::Result<Request> {
+        Request::from_stream(|buf| self.stream.read(buf))
+    }
+
+    pub fn handle_client(&mut self) -> std::io::Result<()> {
+        let request = self.receive()?;
+        let mut transaction = Transaction::new(request, Response::new());
+
+        match transaction.request.method.as_str() {
+            "GET" => {
+                self.process_get_request(&mut transaction)?;
+            },
+            "POST" => {
+                self.process_post_request(&mut transaction)?;
+            },
+            _ => {
+                transaction.response.set_status(&"405 Method Not Allowed");
+                transaction.response.set_header("Allow", "GET, POST");
+                self.send(&transaction.response.to_string().unwrap())?;}
+        }
+        Ok(())
+    }
+
+    fn process_echo_request(&mut self, transaction: &mut Transaction) -> std::io::Result<()> {
+        transaction.response.set_status(&"200 OK");
+        transaction.response.set_body(transaction.request.url.split("/").collect::<Vec<&str>>()[2]);
+        transaction.response.set_header("Content-Type", "text/plain");
+        self.send(&transaction.response.to_string().unwrap())?;
+        Ok(())
+    }
+
+    fn process_user_agent_request(&mut self, transaction: &mut Transaction) -> std::io::Result<()> {
+        transaction.response.set_status(&"200 OK");
+        transaction.response.set_body(&transaction.request.headers.get("User-Agent").unwrap_or(&"".to_string()));
+        transaction.response.set_header("Content-Type", "text/plain");
+        self.send(&transaction.response.to_string().unwrap())?;
+        Ok(())
+    }
+
+    fn process_file_download_request(&mut self, transaction: &mut Transaction) -> std::io::Result<()> {
+        let filename = transaction.request.url.split("/").collect::<Vec<&str>>()[2];
+        let dirname = self.config.download_dir.as_str();
+        let result = std::fs::read_to_string(format!("{}/{}", dirname, filename));
+        match result {
+            Ok(content) => {
+                transaction.response.set_status(&"200 OK");
+                transaction.response.set_body(&content);
+                transaction.response.set_header("Content-Type", "application/octet-stream");
+                self.send(&transaction.response.to_string().unwrap())?;
+            },
+            Err(_) => {
+                transaction.response.set_status(&"404 Not Found");
+                self.send(&transaction.response.to_string().unwrap())?;
+            }
+        }
+        Ok(())
+    }
+
+    fn process_get_request(&mut self, transaction: &mut Transaction) -> std::io::Result<()> {
+
+        if transaction.request.url.starts_with("/echo/") {
+            self.process_echo_request(transaction)?;
+        } else if transaction.request.url.starts_with("/user-agent") {
+            self.process_user_agent_request(transaction)?;
+        } else if transaction.request.url.starts_with("/files"){
+            self.process_file_download_request(transaction)?;
+        } else if transaction.request.url == "/" {
+            self.send(&transaction.response.to_string().unwrap())?;
+        } else {
+            transaction.response.set_status(&"404 Not Found");
+            self.send(&transaction.response.to_string().unwrap())?;
+        }
+        Ok(())
+    }
+
+    fn process_post_request(&mut self, transaction: &mut Transaction) -> std::io::Result<()> {
+        if transaction.request.url.starts_with("/files/") {
+            let filename = transaction.request.url.split("/").collect::<Vec<&str>>()[2];
+            let dirname = self.config.upload_dir.as_str();
+            let mut file = std::fs::File::create(format!("{}/{}", dirname, filename)).unwrap();
+
+            file.write_all(transaction.request.body.as_bytes())?;
+            self.send(&transaction.response.to_string().unwrap())?;
+        } else {
+            transaction.response.set_status(&"404 Not Found");
+            self.send(&transaction.response.to_string().unwrap())?;
+        }
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,117 +1,7 @@
-use std::{env, io::{Read, Write}, net::{TcpListener, TcpStream}, thread};
-use http_server_starter_rust::{Request as HttpRequest, Response as HttpResponse};
+use std::{env, net::{TcpListener}, thread};
+use http_server_starter_rust::{Session, SessionConfig};
 
-// Send response string to the client
-fn send_response(stream: &mut std::net::TcpStream, response: &str) -> std::io::Result<()> {
-    stream.write(response.as_bytes())?;
-    stream.flush()?;
-    Ok(())
-}
 
-// Process GET request
-fn process_get_request(stream: &mut TcpStream, request: HttpRequest) -> std::io::Result<()> {
-    let mut response: HttpResponse = HttpResponse::new();
-    response.set_version(&request.version);
-    response.set_status(&"200 OK");
-
-    if request.url.starts_with("/echo/") {
-        response.set_body(request.url.split("/").collect::<Vec<&str>>()[2]);
-        response.set_header("Content-Type", "text/plain");
-        send_response(stream,&response.to_string().unwrap())?;
-    } else if request.url.starts_with("/user-agent") {
-        response.set_body(&request.headers.get("User-Agent").unwrap_or(&"".to_string()));
-        response.set_header("Content-Type", "text/plain");
-        send_response(stream,&response.to_string().unwrap())?;
-    } else if request.url.starts_with("/files"){
-        let filename = request.url.split("/").collect::<Vec<&str>>()[2];
-        let dirname = get_serving_directory();
-
-        let result = std::fs::read_to_string(format!("{}/{}", dirname, filename));
-        match result {
-            Ok(content) => {
-                response.set_body(&content);
-                response.set_header("Content-Type", "application/octet-stream");
-                send_response(stream,&response.to_string().unwrap())?;
-            },
-            Err(_) => {
-                send_not_found(stream, request)?;
-            }
-        }
-    } else if request.url == "/" {
-        send_response(stream,&response.to_string().unwrap())?;
-    } else {
-        send_not_found(stream, request)?;
-    }
-    Ok(())
-}
-
-fn process_post_request(stream: &mut TcpStream, request: HttpRequest) -> std::io::Result<()> {
-    let mut response: HttpResponse = HttpResponse::new();
-    response.set_version(&request.version);
-    response.set_status(&"201 Created");
-
-    if request.url.starts_with("/files/") {
-        let filename = request.url.split("/").collect::<Vec<&str>>()[2];
-        let dirname = get_serving_directory();
-        let mut file = std::fs::File::create(format!("{}/{}", dirname, filename))?;
-        file.write_all(request.body.as_bytes())?;
-        send_response(stream,&response.to_string().unwrap())?;
-    } else {
-        send_not_found(stream, request)?;
-    }
-
-    Ok(())
-}
-
-// Send 404 Not Found response to the client
-fn send_not_found(stream: &mut TcpStream, request: HttpRequest)-> std::io::Result<()> {
-    let mut response: HttpResponse = HttpResponse::new();
-    response.set_version(&request.version);
-    response.set_status(&"404 Not Found");
-    send_response(stream,&response.to_string().unwrap())
-}
-
-// Send 405 Method Not Allowed response to the client
-fn send_not_allowed(stream: &mut TcpStream, request: HttpRequest)-> std::io::Result<()> {
-    let mut response: HttpResponse = HttpResponse::new();
-    response.set_version(&request.version);
-    response.set_status(&"405 Method Not Allowed");
-    response.set_header("Allow", "GET");
-    send_response(stream,&response.to_string().unwrap())
-}
-
-fn parse_request(stream: &mut TcpStream) -> std::io::Result<HttpRequest> {
-    let reader = |b: &mut[u8]| {stream.read(b)};
-    HttpRequest::from_stream(reader)
-}
-
-fn process_request(stream: &mut TcpStream, request: HttpRequest) -> std::io::Result<()> {
-    match request.method.as_str() {
-        "GET" => {
-            process_get_request(stream, request)?;
-        },
-        "POST" => {
-            process_post_request(stream, request)?;
-        },
-        _ => {
-            send_not_allowed(stream, request)?;
-        }
-    }
-    Ok(())
-}
-
-fn handle_client(stream: &mut TcpStream) -> std::io::Result<()> {
-    let result: Result<HttpRequest, std::io::Error> = parse_request(stream);
-    match result {
-        Ok(request) => {
-            process_request(stream, request)?;
-        }
-        Err(e) => {
-            println!("Error parsing request: {}", e);
-        }
-    }
-    Ok(())
-}
 
 fn get_serving_directory() -> String {
     let args: Vec<String> = env::args().collect();
@@ -125,14 +15,18 @@ fn get_serving_directory() -> String {
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").expect("Failed to create listener");
-    
+    let dirname = get_serving_directory();
+
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
+                let mut session = Session::new(SessionConfig{
+                                                                    download_dir: dirname.clone(),
+                                                                    upload_dir: dirname.clone(),},
+                                                        stream);
                 thread::spawn(move || {
-                    handle_client(&mut stream).unwrap_or(());
-                    stream.shutdown(std::net::Shutdown::Both)
-                        .unwrap_or_else(|e| println!("error: {} in closing connection", e));
+                    session.handle_client().unwrap_or(());
+                    session.close().unwrap_or(());
                 });
             }
             Err(e) => {
